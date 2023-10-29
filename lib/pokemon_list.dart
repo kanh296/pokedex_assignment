902ba7056e8f0e96235441ffdad77a31ed036a68
kanh296 <kieuanhhoang2906@gmail.com>
2023-10-29 18:02:25 +0200

    feat: Create Pokemon list widget with async API fetch and navigation rail to Pokemon detail widget later

---
+++ lib/pokemon_list.dart
@@ -0,0 +1,90 @@
+import 'package:flutter/material.dart';
+import 'pokemon_detail.dart';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+class PokemonList extends StatefulWidget {
+  const PokemonList({super.key});
+
+  @override
+  _PokemonListState createState() => _PokemonListState();
+}
+
+class _PokemonListState extends State<PokemonList> {
+  List<dynamic> pokemonList = [];
+
+  @override
+  void initState() {
+    super.initState();
+    fetchData();
+  }
+
+  Future<void> fetchData() async {
+    final response = await http.get(Uri.parse("https://pokeapi.co/api/v2/pokemon/?limit=10"));
+
+    if (response.statusCode == 200) {
+      final data = json.decode(response.body);
+      setState(() {
+        pokemonList = data['results'];
+      });
+    } else {
+      throw Exception('Failed to load data');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.red,
+        title: const Text(
+          'Pokedex',
+          style: TextStyle(
+            fontSize: 30,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.only(top: 30),
+        child: ListView.builder(
+        itemCount: pokemonList.length,
+        itemBuilder: (context, index) {
+          final pokemon = pokemonList[index];
+          final pokemonName = pokemon['name'];
+          return Padding(
+            padding: const EdgeInsets.all(4.0),
+            child: GestureDetector(
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => PokemonDetail(name: pokemonName),
+                  ),
+                );
+              },
+            child: Container(
+              decoration: BoxDecoration(
+                color: const Color.fromARGB(207, 56, 121, 161),
+                borderRadius: BorderRadius.circular(15),
+              ),
+              child: ListTile(
+                title: Center(
+                  child: Text(
+                    pokemonName,
+                    style: const TextStyle(
+                      color: Colors.white,
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          );
+        },
+      ),
+    ),
+  );
+}
+}
